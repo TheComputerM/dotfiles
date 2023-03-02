8846ddce6e373792f14fd882b86a8b900d49b49e
TheComputerM <mudit.somani00@gmail.com>
2023-03-02 11:34:05 +0530

    increase sleep time

--- .config/hypr/scripts/portals.sh
+++ .config/hypr/scripts/portals.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sleep 1
+sleep 2
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 /usr/libexec/xdg-desktop-portal-hyprland &
-sleep 2
+sleep 3
 /usr/lib/xdg-desktop-portal &
