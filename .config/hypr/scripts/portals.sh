57700f5ce49528002abeb979f97f35127b1e1ecb
TheComputerM <mudit.somani00@gmail.com>
2023-04-14 03:19:25 +0530

    update portals

--- .config/hypr/scripts/portals.sh
+++ .config/hypr/scripts/portals.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sleep 2
+sleep 1
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/libexec/xdg-desktop-portal-hyprland &
-sleep 3
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
 /usr/lib/xdg-desktop-portal &
-
